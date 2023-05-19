65f60557bec25d832d25cd244754938ca5d7ece2
Bjay <jayabjay2018@gmail.com>
2023-05-19 10:06:25 +0300

    Added A header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+import { FavoriteBorder, LoginOutlined, Search } from '@mui/icons-material'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div className='header-items'>
+        <div className='header-item'>
+            <h5>Hello, Sign In to your account <LoginOutlined/></h5>
+            <h3>Check Out This Awesome Movie suggestions!</h3>
+            <div className='avatar'>
+                <img src='https://cdn-icons-png.flaticon.com/512/16/16363.png' alt='' className='avatar-icon'/>
+            </div>
+        </div>
+
+        <div className='header-item'>
+            <FavoriteBorder/>
+            <h5>My List</h5>
+        </div>
+
+
+        {/* <div className='header-item'>
+            <div className='header-item-search'>
+                <input type='text' placeholder='Search Movies' />
+                <Search/>
+            </div>
+        </div> */}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
